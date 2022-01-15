ea2a72a0a348e2e19b6704db2307206cd78e443c
Paolo <paolo.ghidoni82@gmail.com>
2022-01-15 16:23:44 +0000

    basic styling

--- handlers/renderPage.js
+++ handlers/renderPage.js
@@ -10,6 +10,9 @@ const renderPage = (allPosts) => {
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <style>
+@import url('https://fonts.googleapis.com/css2?family=Exo+2:wght@300;400;600&display=swap');
+</style>
     <link rel="stylesheet" href="./styles.css">
     <title>Moonlighter</title>
   </head>
@@ -17,15 +20,18 @@ const renderPage = (allPosts) => {
 
     <div class="app-wrapper center width-md stack-md">
 
+      <h1>Moonlighter</h1>
+      <h2>Send your wishes to the moon!</h2>
+
       <form class="post-add-form stack-sm" action="/add-post" method="POST">
       
         <label for="user">Name</label>
         <input type="text" name="user" id="user" class="user" required>
 
         <label for="message">Message</label>
-        <textarea rows="5" cols="30" name="message" class="message"></textarea>
+        <textarea rows="5" cols="30" name="message" class="message" required></textarea>
 
-        <button type="submit" class="btn post__submit-btn"> Send to🌔</button>
+        <button type="submit" class="btn post__submit-btn"> Send to 🌔</button>
 
       </form>
 
